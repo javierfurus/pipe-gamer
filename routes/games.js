070b0de9c4de8aafab0850af55f4f25f047ee2c1
javierfurus <javierfurus@gmail.com>
2020-09-11 09:47:16 +0200

    Add your favorite games to your profile or delete them if they hurt your feelings

--- routes/games.js
+++ routes/games.js
@@ -4,8 +4,46 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-  const games = await knex('game_list').select();
+  const games = await knex('game_list')
+    .leftJoin('collection', 'collection.game_id', 'game_list.game_id')
+    .select('collection.*', 'game_list.*');
   res.render('games', { title: 'Games', games });
 });
-
+router.use((req, res, next) => {
+  // Itt megnézzük, hogy a hívás, amit kapunk, az mi
+  if (req.query._method === 'DELETE') {
+    // Ezzel megváltoztatjuk az eredeti metódust DELETE-re
+    req.method = 'DELETE';
+    // Majd visszalakítjuk az eredeti URL-t, pl. /todos/1
+    req.url = req.path;
+  }
+  if (req.query._method === 'PUT') {
+    // Putnál ismét megcsináljuk ugyanezt
+    req.method = 'PUT';
+    req.url = req.path;
+  }
+  if (req.query._method === 'POST') {
+    // Putnál ismét megcsináljuk ugyanezt
+    req.method = 'POST';
+    req.url = req.path;
+  }
+  next();
+});
+router.post('/:id', async (req, res, next) => {
+  const collection = {
+    player_id: 1,
+    game_id: req.params.id,
+    progress: 0,
+    favorite: 1
+  };
+  await knex('collection').insert(collection);
+  res.redirect('back');
+});
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  await knex('collection')
+    .where('game_id', id)
+    .del();
+  res.redirect('back');
+});
 module.exports = router;
